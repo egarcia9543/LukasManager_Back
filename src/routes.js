800f0c067c651263aad420876cc7c29ec77e1d78
egarcia9543 <egarcia9543@soy.sena.edu.co>
2024-06-18 01:00:28 -0500

    feat: generate login controller and route

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('./app/controller/user.controller');
 
 router.post('/sign-up', userController.registerUser);
+router.post('/login', userController.login);
 
 module.exports = router;
